d59799ae1b2bd0367c431884ec59b438b328389b
Khrystyna-Hnatiuk <114655529+Khrystyna-Hnatiuk@users.noreply.github.com>
2023-07-17 17:53:59 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -52,3 +52,30 @@ function openLogoBlogs(){
     
 }
 
+
+
+
+// function setscrollBy(){
+//     window.scrollBy(0,2800);
+//     const windScroll=window.pageYOffset;
+//     console.log(windScroll);
+// }
+// function setscrollBy(){
+//     window.scrollTo({
+// top:2800,
+// left: 0,
+// behavior:'smooth'
+//     })
+//     // const windScroll=window.pageYOffset;
+//     // console.log(windScroll);
+// }
+
+function setscrollBy(top){
+    let sectSelected = document.querySelector('.section-3');
+    sectSelected.scrollIntoView({
+
+        block:"start",
+        inline:"nearest",
+        behavior:'smooth'
+    });
+}
